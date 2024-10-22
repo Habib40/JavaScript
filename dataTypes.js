5e1ec756b29a8377f6d04ac92382b10f4285cd60
Habib-Jaman <habibkb5080@gmail.com>
2024-10-22 07:05:24 +0600

    js commit

---
+++ dataTypes.js
@@ -0,0 +1,78 @@
+// let myName = 'Habib jaman'
+// console.log(typeof myName)
+
+
+// let num = 6;
+// let num2 = 3;
+
+// console.log(Boolean(num)+num2)
+
+// let num = 6;
+// let num2 = 3;
+
+// console.log(String(num)+num2)
+
+// let num = "6";
+// let num2 = "3";
+
+// console.log(Number(num) + num2)
+// // result is commming 63 why?
+
+
+// let num = "6";
+// let num2 = "3";
+
+// console.log(Number(num) + Number(num2))
+// //   now result is 9
+
+
+// let num = "6";
+// let num2 = "3";
+// let myName = 'Habib Jaman';
+
+
+// console.log(Number(num) + Number(num2) + Boolean(myName))
+// // result is 9 and "True is 1 so final result is 10"
+
+// let num = "6";
+// let num2 = "3";
+// let myName = Number('Habib Jaman');
+
+
+// console.log(Number(num) + Number(num2) + Boolean(myName))
+//   // result is 9 and "False is 0 so final result is 9"
+
+
+
+// let x = "Volvo" + (16 + 4);
+// console.log(x)
+// //Now result is Volvo20
+
+
+// let x = "Volvo" + 16 + 4;
+// console.log(x)
+// //Now result is Volvo164 becouse 16&4 = 20 but in string case it is being 16 and 4 and then final result is volvo164
+
+
+// let x;       // Now x is undefined
+// x = 5;       // Now x is a Number
+// x = "John";  // Now x is a String
+// console.log(x+(x));
+// // x is printing twise
+
+
+// let x;       // Now x is undefined
+// x = 5;       // Now x is a Number
+// x = Number("John");  // Now x is a String
+// console.log(x + x)
+// // why it is NaN
+
+
+// let x;       // Now x is undefined
+// x = String(5);       // Now x is a Number
+// x = "John";  // Now x is a String
+// console.log(x + String(x));
+// // Why it is johnjohn
+
+
+console.log('hello world');
