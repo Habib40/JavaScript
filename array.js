80573d62aa913b3489b0a9b1e1462fd2aaa11335
Habib-Jaman <habibkb5080@gmail.com>
2024-10-25 21:47:49 +0600

    Array

---
+++ array.js
@@ -0,0 +1,25 @@
+// const user = {
+//     id : 12,
+//     Name: 'John Doe',
+//     age: 65,
+//     location: 'Dhaka',
+//     education:{
+//           degree: 'Masters'
+     
+//     },
+// }
+
+// // const { id, name,education:{degree} } = user;
+// // console.log("Student Id " + id, ",name " + Name + ":" + "Degree " + degree);
+// // // Array destructing
+// function printUserInfo({ id, Name, education: { degree } }) {
+
+//     console.log(`User information : Id - ${id},Name:${Name},Education:${degree}`);
+// }
+// printUserInfo(user)
+// // function printUserInfo({ id, Name, age }) {
+// //     console.log(`User Info - ID: ${id}, Name: ${Name}, Age: ${age}`);
+// // }
+
+// // // Passing the user object to the function
+// // printUserInfo(user);
