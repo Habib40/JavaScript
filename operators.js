8a8861a80c603bcd6942239e7f1b696865b1adbc
Habib-Jaman <habibkb5080@gmail.com>
2024-10-25 11:48:46 +0600

    second commit

---
+++ operators.js
@@ -0,0 +1,62 @@
+// let num1 = 10;
+// let num2 = 20;
+
+// console.log(num1 + num2);
+// // result 30
+
+
+// let num1 = 10;
+// let num2 = 20;
+
+// console.log(num1 >= num2);
+// // result false
+
+
+// let num1 = 10;
+//  num1 = ++num1
+
+// console.log(num1);
+// // result 11
+
+// let num1 = 10;
+// num1++
+
+// console.log(num1);
+// // this result also 11
+
+// And or not operators
+// let age = 50;
+// let location = 'Dkaka';
+
+// console.log(age > 50 && location == 'Dhaka');
+// // // this false
+
+
+// let age = 50;
+// let location = 'Dhaka';
+
+// console.log(age >= 50 && location == 'Dhaka');
+// // // this is true
+
+// let age = 50;
+// let location = 'Dhaka'; // Corrected location
+
+// let result = (age >= 50 && location === 'Dhaka') ? 'Conditions met' : 'Conditions not met';
+// console.log(result);
+
+
+// let age = 18;
+
+// let eligibility = (age >= 18) ? 'Eligible to vote' : 'Not eligible to vote';
+// console.log(eligibility);
+
+
+// Ternary operator
+// let num1 = 10;
+// let num2 = 20;
+
+
+// let bigNumber = (num1 > num2) ? num1 : num2
+// let smallNumber = (num1 > num2) ? num2 : num1
+
+// console.log('The big number is ' + bigNumber, 'The small number is '+ smallNumber);
